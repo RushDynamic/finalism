bbe1243e6ab4cde3eb99b62191e15f67fcd6e729
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-09-06 03:34:26 +0530

    Added skeleton Spring Boot server

---
+++ finalism-server/src/main/java/com/rushdynamic/finalism/FinalismApplication.java
@@ -0,0 +1,13 @@
+package com.rushdynamic.finalism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalismApplication.class, args);
+	}
+
+}
---
+++ finalism-server/src/test/java/com/rushdynamic/finalism/FinalismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushdynamic.finalism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
