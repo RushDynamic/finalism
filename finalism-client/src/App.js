4990aa289a9b98c113da8ac9b74b1e847eb6d991
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-09-30 14:01:28 +0530

    Added semantic UI css

--- finalism-client/src/App.js
+++ finalism-client/src/App.js
@@ -1,9 +1,12 @@
 import Header from "./components/header/Header";
+import Submit from "./components/submit/Submit";
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div>
       <Header />
+      <Submit />
     </div>
   );
 }
