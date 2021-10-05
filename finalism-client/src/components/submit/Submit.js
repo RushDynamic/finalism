2509d4658b69287f5452bb1ae9d6e9e976690c73
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-05 08:21:39 +0530

    Added result layout changes

--- finalism-client/src/components/submit/Submit.js
+++ finalism-client/src/components/submit/Submit.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { Input, Segment } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Input, Segment, Header, Icon } from "semantic-ui-react";
 import '../../styles/submit.scss';
 
+const ResultSegment = (resultFetched) => {
+    return (resultFetched && (
+        <Segment placeholder>
+            <Header icon>
+                <Icon name="world" />
+                Here's your shortened URL
+            </Header>
+        </Segment>)
+    )
+}
+
 function Submit() {
+    const [resultFetched, setResultFetched] = useState(false);
+
     return (
         <div className='submit-container'>
             <div className='submit-inner-container'>
                 <Input className='submit-input-field' action='Submit' placeholder='Enter a URL...' />
+            </div>
+            <div className='result-container'>
+                {ResultSegment(resultFetched)}
             </div>
         </div>)
 }
