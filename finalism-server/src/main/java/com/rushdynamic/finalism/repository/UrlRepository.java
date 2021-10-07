fb097e61d3f369b88a588b95603b2e296f3c620a
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-07 11:40:21 +0530

    Added JPA method for findByShortUrl

--- finalism-server/src/main/java/com/rushdynamic/finalism/repository/UrlRepository.java
+++ finalism-server/src/main/java/com/rushdynamic/finalism/repository/UrlRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rushdynamic.finalism.entity.UrlEntity;
 
 public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
-	
+	UrlEntity findByShortUrl(String shortUrl);
 }
