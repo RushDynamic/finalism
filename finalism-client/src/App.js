24ae6c94d7e57f3cd8568d475cec8040ed7c9756
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-07 20:16:15 +0530

    Added react-router

--- finalism-client/src/App.js
+++ finalism-client/src/App.js
@@ -1,14 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Submit from "./components/submit/Submit";
+import Redirect from "./components/Redirect";
 import 'semantic-ui-css/semantic.min.css'
 import './styles/app.scss';
+
 
 function App() {
   return (
     <div>
       <div className="main-container">
-        <Header />
-        <Submit />
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Header />
+              <Submit />
+            </Route>
+            <Route path='/:url'>
+              <Header />
+              <Redirect />
+            </Route>
+          </Switch>
+        </Router>
+
       </div>
     </div>
   );
