a5613be59676e4a4b6d01c3ccfbd987294a792dd
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-07 20:18:07 +0530

    Updated CrossOrigin annotation

--- finalism-server/src/main/java/com/rushdynamic/finalism/controller/FinalismAppController.java
+++ finalism-server/src/main/java/com/rushdynamic/finalism/controller/FinalismAppController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rushdynamic.finalism.dto.OriginalUrlOutputDto;
 import com.rushdynamic.finalism.dto.OriginalUrlResponseOutput;
 import com.rushdynamic.finalism.dto.ResponseOutput;
 import com.rushdynamic.finalism.dto.ShortenUrlInputDto;
@@ -18,13 +17,13 @@ import com.rushdynamic.finalism.dto.ShortenUrlOutputDto;
 import com.rushdynamic.finalism.dto.ShortenUrlResponseOutput;
 import com.rushdynamic.finalism.service.ShortenUrlService;
 
+@CrossOrigin
 @RestController
 public class FinalismAppController {
 
 	@Autowired
 	private ShortenUrlService shortenUrlService;
 	
-	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping("/shorten")
 	public ShortenUrlResponseOutput shorten(@RequestBody ShortenUrlInputDto shortenUrlInput) {
 		ShortenUrlResponseOutput shortenUrlOutputResponse = new ShortenUrlResponseOutput();
@@ -36,8 +35,7 @@ public class FinalismAppController {
 		return shortenUrlOutputResponse;
 	}
 	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/{shortUrl}")
+	@GetMapping("/fetch/{shortUrl}")
 	public OriginalUrlResponseOutput fetchOriginalUrl(@PathVariable("shortUrl") String shortUrl) throws URISyntaxException {
 		ResponseOutput response = new ResponseOutput("200", "Successfully fetched original URL");
 		return new OriginalUrlResponseOutput(shortenUrlService.fetchOriginalUrl(shortUrl), response);
