74134bd2ab69dfb1c2f3bd0511df2b0e94ef2935
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-07 11:39:48 +0530

    Added constructor

--- finalism-server/src/main/java/com/rushdynamic/finalism/dto/ResponseOutput.java
+++ finalism-server/src/main/java/com/rushdynamic/finalism/dto/ResponseOutput.java
@@ -4,6 +4,14 @@ import lombok.Data;
 
 @Data
 public class ResponseOutput {
+	
 	private String status;
-	private String message; 
+	
+	private String message;
+
+	public ResponseOutput(String status, String message) {
+		super();
+		this.status = status;
+		this.message = message;
+	} 
 }
