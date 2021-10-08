d2cab732bdb2065462397ae297ea314fafa2f800
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-08 19:21:51 +0530

    UI changes

--- finalism-client/src/components/submit/ResultSegment.js
+++ finalism-client/src/components/submit/ResultSegment.js
@@ -1,10 +1,13 @@
-import { Segment, Header, Button, Icon } from "semantic-ui-react";
+import { Segment, Header, Button } from "semantic-ui-react";
 const ResultSegment = (resultFetched, shortUrl) => {
     return (resultFetched && (
         <Segment placeholder>
             <Header icon>
-                <Button size='tiny' icon='copy outline' style={{ backgroundColor: '#F9FAFB' }} onClick={() => { navigator.clipboard.writeText(shortUrl) }} />
-                {`Here's your shortened URL: ${shortUrl}`}
+                <Button size='tiny' icon='copy outline' style={{ backgroundColor: '#F9FAFB', marginBottom: '2vh', marginTop: '2vh' }} onClick={() => { navigator.clipboard.writeText(shortUrl) }} />
+                <div>
+                    <h4>Here's your shortened URL:</h4>
+                    <h2 style={{ marginTop: '0vh' }}>{shortUrl}</h2>
+                </div>
             </Header>
         </Segment>)
     )
