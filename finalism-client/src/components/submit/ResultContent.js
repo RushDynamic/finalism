35a9c38f61176c4c98beb4fb8ed6e381e94bd94a
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-12 18:36:36 +0530

    Restructured Result Container

--- finalism-client/src/components/submit/ResultContent.js
+++ finalism-client/src/components/submit/ResultContent.js
@@ -10,23 +10,25 @@ function ErrorContent(errorMsg) {
 
 function SuccessContent(shortUrl) {
     return (
-        <>
-            <h4>Here's your shortened URL:</h4>
-            <h2 style={{ marginTop: '0vh' }}>{shortUrl}</h2>
-            <button className="result-copy-btn" type="button" onClick={() => navigator.clipboard.writeText(shortUrl)}>COPY</button>
-        </>
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+            <div>
+                <h4 style={{ marginBottom: '0.5vh', color: '#454545' }}>Here's your shortened URL:</h4>
+                <h2 style={{ marginTop: '0vh', color: 'black', textShadow: ' 1px 1px #ffffff' }}>{shortUrl}</h2>
+            </div>
+            <button style={{ marginLeft: '4vh' }} className="result-copy-btn" type="button" onClick={() => navigator.clipboard.writeText(shortUrl)}>COPY</button>
+        </div>
     )
 }
 
 function ResultContent(props) {
     return (
-        <div>
+        <>
             {
                 (
                     props.error.isError ? ErrorContent(props.error.message) : SuccessContent(props.shortUrl)
                 )
             }
-        </div>
+        </>
     );
 }
 
