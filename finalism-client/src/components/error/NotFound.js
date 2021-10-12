383ebca1f9e367811977407a9df5aaabb613aa3c
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-10-12 18:18:08 +0530

    Updated error style

--- finalism-client/src/components/error/NotFound.js
+++ finalism-client/src/components/error/NotFound.js
@@ -2,7 +2,7 @@ import '../../styles/error.scss'
 
 function NotFound() {
     return (<div className="not-found-container">
-        <h2>That URL could not be found</h2>
+        <h2>Are you lost?</h2>
     </div>)
 }
 
